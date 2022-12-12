7389af2d619955707f149196d53a6f81fb9ec26f
kapilmahajan123 <montyempire123gmail.com>
2022-12-12 14:12:27 +0530

    Added: Ability to Dequeue element from the Queue

--- src/com/Stacks/ds/Queue.java
+++ src/com/Stacks/ds/Queue.java
@@ -8,9 +8,15 @@ package com.Stacks.ds;
 public class Queue {
 	
 	LinkedList list = new LinkedList(); //Declaring linked list
-
+	
+	/*
+	 * Creating Queue method
+	 */
 	public void enQueue(Object data) {
 		list.addFirst(data);
+	}
+	public void deQueue() {
+		list.deleteFirst();
 	}
 	//display method
 	public void display() {
@@ -23,6 +29,13 @@ public class Queue {
 		queue.enQueue(70);
 		queue.enQueue(30);
 		queue.enQueue(56);
+		queue.display();
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		queue.deQueue();
 		queue.display();
 	}
 
